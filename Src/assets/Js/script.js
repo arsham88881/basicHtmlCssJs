5943666e1963473394fe606104bbf0893ac46db6
Arsham <arsham.fadaie@gmail.com>
2023-10-14 14:00:06 +0330

    complete basic part and ready to deploy

--- Src/assets/Js/script.js
+++ Src/assets/Js/script.js
@@ -1,1 +1,12 @@
-console.log("hello script");
+//setup nav
+var navbar = document.querySelector("nav.navbar");
+var navOpenBtn = document.querySelector(".nav-btn");
+var navCloseBtn = document.querySelector(".navbar-close-btn");
+//close navbar
+navCloseBtn.addEventListener("click", () => {
+  navbar.classList.remove("show-navbar");
+});
+//open navbar
+navOpenBtn.addEventListener('click',()=>{
+    navbar.classList.add("show-navbar");
+});

3425b0b63513a32b02f7e880afab92452346ea5f
Arsham <arsham.fadaie@gmail.com>
2023-10-16 01:11:51 +0330

    todo app theme switcher complete

--- Src/assets/Js/script.js
+++ Src/assets/Js/script.js
@@ -12,3 +12,24 @@ navOpenBtn.addEventListener("click", () => {
   navbar.classList.add("show-navbar");
 });
 /*basices part end*/
+// //variables js on todo app 
+// const addTodoBtn = document.getElementById("add-todo-btn");
+// const TodoInput = document.getElementById("todo-input");
+const bodyTag = document.querySelector("body");
+const IconThemSwitcher = document.querySelector(".banner-text-icon-todo i"); 
+const BtnThemSwitcher = document.querySelector(".banner-text-icon-todo");
+// get todo part Start 
+// addTodoBtn.addEventListener('click',()=>{
+
+// });
+// end todo part 
+// dark and light switcher them
+
+BtnThemSwitcher.addEventListener('click',()=>{
+  bodyTag.classList.toggle('light');
+  bodyTag.classList.toggle('dark');
+
+  IconThemSwitcher.classList.toggle("bi-brightness-high");
+  IconThemSwitcher.classList.toggle("bi-moon-stars-fill");
+
+});
